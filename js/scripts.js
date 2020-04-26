61040f4d16dfc1b267fa1a4b87f556b295106c87
Klein, Philipp <mail@kleinphilipp.de>
2020-04-26 15:53:19 +0200

    first playable version * setup is now saved locally * tiles are now randomly assigned to teams with a fixed ratio * the starter team is selected randomly (one more tile than the other team) * the amount of assassins can be configured ** no assassin at all ** a single assassin ** number of assassins scaled with the size of the board (with fixed assassin to tile ration of 1 / 25)

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 const boxClass = "box"
-const boxName = "boxTile"
+const boxName = "decoderTile"
 
-function changeColorById() {
-    let boxId = document.getElementById("boxSelector").value - 1
-    let selectedBox = document.getElementById(boxName + boxId);
-    selectedBox.style.backgroundColor = "blue";
+function colorizeDecoderTiles(boxIds, startIndex, indexCount, color) {
+    let lastIndex = startIndex;
+    for (let i = startIndex; i < startIndex + indexCount; i++) {
+        let selectedBox = document.getElementById(boxIds[i]);
+        selectedBox.style.backgroundColor = color;
+        lastIndex = i;
+    }
+    return lastIndex;
 }
 
 function createBox(id) {
@@ -14,33 +18,94 @@ function createBox(id) {
     return newBox;
 }
 
-function addBoxes() {
+function updateDecoder() {
     let columnCount = document.getElementById("columnCount").value;
     let rowCount = document.getElementById("rowCount").value;
-    let count = columnCount * rowCount;
-    
-    let grid = updateMainGrid(columnCount);
+    saveSettings(rowCount, columnCount);
 
+    let count = columnCount * rowCount;
+
+    let grid = updateDecoderGrid(columnCount);
+
+    let boxIds = [];
     for (let i = 0; i < count; i++) {
         let boxIndex = i;
         let box = createBox(boxName + boxIndex);
         grid.appendChild(box);
+        boxIds.push(box.id);
+        boxIds.sort(function (a, b) { return 0.5 - Math.random() });
     }
-    return false;
+
+    let teamOneTileCounts = count * 0.36;
+    let teamTwoTileCounts = teamOneTileCounts - 1;
+
+    let assassinCount = count * 0.04;
+
+    switch (getAssassinSetting()) {
+        case "noAsssassin":
+            assassinCount = 0;
+            break;
+        case "singleAsssassin":
+            assassinCount = 1
+            break;
+        default:
+        // code block
+    }
+
+
+colors = ["blue", "red"]
+colors.sort(function (a, b) { return 0.5 - Math.random() });
+document.getElementById("startingTeam").style.backgroundColor = colors[0];
+
+startIndex = 0;
+let lastIndexTeamOne = colorizeDecoderTiles(boxIds, startIndex, teamOneTileCounts, colors[0]);
+let lastIndexTeamTwo = colorizeDecoderTiles(boxIds, lastIndexTeamOne + 1, teamTwoTileCounts, colors[1]);
+let lastIndexAssassin = colorizeDecoderTiles(boxIds, lastIndexTeamTwo + 1, assassinCount, "black");
+
+return false;
 }
 
-function updateMainGrid(columns) {
-    let grid = document.getElementById("mainGrid");
+function updateDecoderGrid(columns) {
+    let grid = document.getElementById("decoderGrid");
     grid.innerHTML = "";
-    grid.style.gridTemplateColumns = createGridcolumnsConfig(columns);
+    grid.style.gridTemplateColumns = createGridcolumnsPropertyValue(columns);
     return grid;
 }
 
-function createGridcolumnsConfig(columns) {
+function createGridcolumnsPropertyValue(columns) {
     var config = "";
     for (let i = 0; i < columns - 1; i++) {
         config += "1fr "
     }
     config += "1fr"
     return config;
+}
+
+function saveSettings(rowCount, columnCount) {
+    localStorage.rowCount = Number(rowCount);
+    localStorage.columnCount = Number(columnCount);
+}
+
+
+function initialize() {
+    if (localStorage.rowCount) {
+        document.getElementById("rowCount").value = localStorage.rowCount;
+    }
+    if (localStorage.columnCount) {
+        document.getElementById("columnCount").value = localStorage.columnCount;
+    }
+    updateDecoder();
+}
+
+function getAssassinSetting() {
+    let assassinSetupRadioGroup = document.getElementsByName("assassinSetup")
+    var selectedSetting;
+
+    for (let i = 0; i < assassinSetupRadioGroup.length; i++) {
+        if (assassinSetupRadioGroup[i].checked) {
+            selectedSetting = assassinSetupRadioGroup[i].value;
+        }
+    }
+
+    return selectedSetting;
 }
